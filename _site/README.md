e2e637c556d9d99d4a6de7298bc12164d2ecd6e1
Patrick Huber <patrickhuber@vpn75.cs.ubc.ca>
2022-02-08 15:20:02 -0800

    gitignore

--- _site/README.md
+++
@@ -1,3 +0,0 @@
-# nlpgroupwebsite
-
-This is the repository to create the new UBC NLP group website
--- _site/assets/js/custom.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.sidenav');
-  var instances = M.Sidenav.init(elems, {});
-});
